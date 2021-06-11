1d8314ffa0994378dc73e27c209fbb5cb442c86e
txwizard <txwizard@gmail.com>
2021-06-10 19:41:20 -0500

    Refresh WizardWrx .NET API Libraries as of 10 June 2021.

--- ConsoleAppAids3/Properties/AssemblyInfo.cs
+++ ConsoleAppAids3/Properties/AssemblyInfo.cs
@@ -101,5 +101,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "8.0.536.0" )]
-[assembly: AssemblyFileVersion ( "8.0.536.0" )]
+[assembly: AssemblyVersion ( "8.0.537.0" )]
+[assembly: AssemblyFileVersion ( "8.0.537.0" )]
