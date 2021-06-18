a0226edb133e3836832477f5f5622ce10accdb07
txwizard <txwizard@gmail.com>
2021-06-18 02:40:04 -0500

    Refresh WizardWrx .NET API Libraries as of 19 June 2021.

--- TestStand/Properties/AssemblyInfo.cs
+++ TestStand/Properties/AssemblyInfo.cs
@@ -83,4 +83,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "7.2.549.0" )]
+[assembly: AssemblyVersion ( "7.2.550.0" )]
