906c9cc41a292d2a257f46e9187505ce1ed896d4
txwizard <txwizard@gmail.com>
2022-03-08 23:28:29 -0600

    Dependency Graph Refresh 08 March 2022

--- TestStand/Properties/AssemblyInfo.cs
+++ TestStand/Properties/AssemblyInfo.cs
@@ -61,7 +61,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ( "" )]
 [assembly: AssemblyCompany ( "David A. Gray" )]
 [assembly: AssemblyProduct ( "WizardWrx .NET Utility Libraries" )]
-[assembly: AssemblyCopyright ( "Copyright 2014-2021, David A. Gray" )]
+[assembly: AssemblyCopyright ( "Copyright 2014-2022, David A. Gray" )]
 [assembly: AssemblyTrademark ( "This library is distributed under a three-clause BSD license." )]
 [assembly: AssemblyCulture ( "" )]
 
@@ -83,4 +83,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "7.2.560.0" )]
+[assembly: AssemblyVersion ( "7.2.561.0" )]
